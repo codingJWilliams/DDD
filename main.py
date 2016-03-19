1325beb04b9fd3f0906a5131c973564b97bd06a9
Angus Goody <angus.goody@gmail.com>
2016-03-19 12:00:04 +0000

    Removed Passwords

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from tkinter import *
 
 window=Tk()
 window.geometry("400x200")
+window.title("DDD")
+
 #=======================Canvas'==========
 
 #========Startup Canvas===
@@ -13,12 +15,23 @@ openCanvas=Canvas(window,width=200,height=200,relief=None,highlightthickness=0)
 openCanvas.pack(expand=True)
 
 Label(openCanvas,text="Username").grid(row=0,column=0)
-Label(openCanvas,text="Password").grid(row=1,column=0)
 
 userNameEntry=Entry(openCanvas)
 userNameEntry.grid(row=0,column=1)
 
-passwordEntry=Entry(openCanvas,show="*")
-passwordEntry.grid(row=1,column=1)
+
+
+#=======================FUNCTIONS=============
+
+#Function that checks the user's credentials
+def checkCredentials(event):
+
+    #Gets the credentials from Entry
+    userNameString=userNameEntry.get()
+    
+
+
+#=============BINDINGS============
+userNameEntry.bind("<Enter>",checkCredentials)
 
 window.mainloop()
