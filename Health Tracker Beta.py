bc108fd86038c5705bea164c0bcdd1515ba53b2a
Angus Goody <angus.goody@gmail.com>
2016-03-20 10:55:59 +0000

    Added Health Tracker Beta

---
+++ Health Tracker Beta.py
@@ -0,0 +1,169 @@
+
+#Health Tracker Beta
+#Angus Goody
+
+from tkinter import *
+from tkinter import messagebox
+
+window=Tk()
+window.geometry("300x200")
+window.title("Health Tracker")
+
+mainMenu=Menu(window)
+window.config(menu=mainMenu)
+
+#===========================MENUS=================
+fileMenu=Menu(mainMenu)
+
+#==========================CANVAS'=================
+
+#====Login CANVAS====
+loginCanvas=Canvas(window,width=200,height=200,relief=None,highlightthickness=0)
+
+Label(loginCanvas,text="Username: ").grid(row=0,column=0)
+
+userNameEntry=Entry(loginCanvas)
+userNameEntry.grid(row=0,column=1,padx=5)
+
+#===View Canvas=====
+
+viewCanvas=Canvas(window,width=200,height=200,relief=None,highlightthickness=0)
+showNameVar=StringVar()
+
+showInfoFrame=Frame(viewCanvas)
+showInfoFrame.pack()
+
+Label(showInfoFrame,text="Name:            ",justify=LEFT).grid(row=0,column=0)
+Label(showInfoFrame,text="Height (cm) : ",justify=LEFT).grid(row=1,column=0)
+Label(showInfoFrame,text="Weight (kg) : ",justify=LEFT).grid(row=2,column=0)
+
+showNameEntry=Entry(showInfoFrame,justify=CENTER)
+showNameEntry.grid(row=0,column=1)
+
+showHeightEntry=Entry(showInfoFrame,justify=CENTER)
+showHeightEntry.grid(row=1,column=1)
+
+showWeightEntry=Entry(showInfoFrame,justify=CENTER)
+showWeightEntry.grid(row=2,column=1)
+
+
+#==========================ARRAYS================
+canvasArray=[loginCanvas,viewCanvas]
+userNameArray=[]
+healthDataArray=[]
+currentViewArray=[]
+#=========================FUNCTIONS================
+def loadCanvas(currentCanvas):
+	for canvas in canvasArray:
+		if canvas != currentCanvas:
+			canvas.pack_forget()
+			
+	currentCanvas.pack(expand=True)
+
+def askMessage(pre,message):
+	try:
+		messagebox.showinfo(pre,message)
+	except:
+		print(message)
+def getContentFromFile(path):
+	try:
+		file=open(path,"r")
+	except:
+		askMessage("Error","Could not find file")
+	else:
+		content=file.readlines()
+		file.close()
+		newContent=[]
+		for line in content:
+			words=line.split()
+			if len(words) > 0:
+				line=line.rstrip()
+				newContent.append(line)
+			
+		return newContent
+def getHealthData():
+        global userNameArray
+        global healthDataArray
+        content=getContentFromFile("usernames.txt")
+        if content != None:
+                userNameArray=[]
+                        
+                #Finds Usernames
+                for segment in content:
+                        words=segment.split(",")
+                        name=words[0]
+                        userNameArray.append(name)
+                        words.remove(name)
+                        temp=[[name],words]
+                        healthDataArray.append(temp)
+		
+
+def checkCredentials():
+	global currentViewArray
+	getHealthData()
+	
+	data=str(userNameEntry.get())
+	found=False
+	for item in healthDataArray:
+		nameSeg=item[0]
+		name=nameSeg[0]
+
+		data=data.capitalize()
+		name=name.capitalize()
+		
+		if data == name:
+			found=True
+			askMessage("Found", "Infomation Found")
+			currentViewArray=item
+			showInfo()
+			break
+			
+	if found == False:
+		askMessage("Not Found", "Go Away")
+
+def insertEntry(entry,message):
+        entry.delete(0,END)
+        entry.insert(END,message)
+
+def showInfo():
+        loadCanvas(viewCanvas)
+
+        #Gets name from current view
+        nameSegment=currentViewArray[0]
+        userName=nameSegment[0]
+        #Data Section
+        dataSegment=currentViewArray[1]
+        
+
+        #Add infomation about userName to screen
+        insertEntry(showNameEntry,userName.capitalize())
+                
+        #Rest of infomation
+
+        print("Data is",dataSegment)
+        try:
+                userWeight=dataSegment[0]
+                userHeight=dataSegment[1]
+        except:
+                insertEntry(showWeightEntry,"Unknown")
+                insertEntry(showHeightEntry,"Unknown")
+        else:
+                insertEntry(showWeightEntry,userWeight)
+                insertEntry(showHeightEntry,userHeight)
+                
+        
+		
+#========================MENU COMMANDS===========
+
+mainMenu.add_cascade(label="File",menu=fileMenu)
+mainMenu.add_command(label="New")
+fileMenu.add_command(label="Login",command=lambda canvas=loginCanvas: loadCanvas(canvas))
+
+#===================BUTTONS====================
+overWriteButton=Button(viewCanvas,text="Overwrite",relief=FLAT)
+overWriteButton.pack(pady=9)
+#===================RETURN COMMANDS============
+getHealthData()
+#===================BINDINGS COMMANDS============
+userNameEntry.bind("<Return>",lambda event: checkCredentials())
+window.mainloop()
